899716e7281bdd65b49ef731716ff1c2d60f04ba
Dave Townsend <dtownsend@oxymoronical.com>
2013-11-29 12:02:16 -0800

    Use hostname as device for wire-1 sensors.

--- w1devices.py
+++ w1devices.py
@@ -1,39 +1,50 @@
 #! /usr/bin/env python
 
 import os, re
+from socket import getfqdn
 from time import time
 
 BUS = "/sys/bus/w1/devices"
 SLAVE = "w1_slave"
+DEVICE = getfqdn()
 
 VALID = re.compile("^([0-9a-fA-F]{2})-[0-9a-fA-F]{12}$")
 
 def read_data(config, device):
-    match = VALID.match(device)
-    if match.group(1) == "28":
-        return read_temperature(device)
-    raise Error("Unknown device type")
+    results = []
+
+    if device != DEVICE:
+        return results
+
+    names = os.listdir(BUS)
+    for sensor in names:
+        if not os.path.isdir(os.path.join(BUS, sensor)):
+            continue
+        match = VALID.match(sensor)
+        if not match:
+            continue
+        if match.group(1) == "28":
+            read_temperature(results, sensor)
+
+    return results
+
 
 def devices(config):
-    names = os.listdir(BUS)
-    for name in names:
-        if not os.path.isdir(os.path.join(BUS, name)):
-            continue
-        if not VALID.match(name):
-            continue
-        yield name
+    return [DEVICE]
 
-def read_temperature(device):
-    tfile = open(os.path.join(BUS, device, "w1_slave"))
+def read_temperature(results, sensor):
+    tfile = open(os.path.join(BUS, sensor, "w1_slave"))
     text = tfile.read()
     tfile.close()
+
     temperature_data = text.split()[-1]
     temperature = float(temperature_data[2:])
     temperature = temperature / 1000
-    return [{
-        "device": "w1-%s" % device,
-        "sensor": "0",
+
+    results.append({
+        "device": DEVICE,
+        "sensor": "w1-%s" % sensor,
         "type": "temperature",
         "time": int(time()),
         "value": temperature
-    }]
+    })
