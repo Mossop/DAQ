bf48b2021290950f268b4503d80ea19a90268cfd
Dave Townsend <dtownsend@oxymoronical.com>
2014-01-01 14:53:17 -0800

    Support parsing CSV files for SCE data.

--- sce.py
+++ sce.py
@@ -5,7 +5,6 @@ import os
 import re
 import json
 from time import time
-from xml.etree import ElementTree
 from pytz import timezone, utc
 from datetime import datetime
 
@@ -15,6 +14,8 @@ def to_epoch(time):
     return delta.seconds + delta.days * 24 * 3600
 
 def read_xml(fname, device):
+    from xml.etree import ElementTree
+
     results = []
 
     tree = ElementTree.parse(fname)
@@ -34,6 +35,34 @@ def read_xml(fname, device):
 
     return results
 
+def read_csv(fname):
+    from csv import DictReader, excel
+
+    results = []
+
+    dialect = excel()
+    dialect.skipinitialspace = True
+
+    lines = open(fname).readlines()
+    header = DictReader(lines[0:2], dialect = dialect).next()
+    device = header["Service Account"]
+
+    data = DictReader(lines[3:], dialect = dialect)
+
+    for datum in data:
+        time = datetime.strptime(datum["Interval Date & Time"], "%m/%d/%Y %I:%M %p ")
+        time = time.replace(tzinfo = timezone("America/Los_Angeles"))
+        results.append({
+            "device": "sce-" + device,
+            "sensor": "electricity",
+            "type": "energy",
+            "time": to_epoch(time),
+            "duration": 3600,
+            "value": float(datum["kWh Delivered"])
+        })
+
+    return results
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Syntax: sce.py <datafile>")
@@ -47,6 +76,9 @@ if __name__ == "__main__":
     result = re.match(r"SCE_Usage_([\d-]+)_[\d-]+_to_[\d-]+\.xml", os.path.basename(fname))
     if result is not None:
         results = read_xml(fname, result.group(1))
+        print("\n".join([json.dumps(r) for r in results]))
+    elif fname[-4:] == ".csv":
+        results = read_csv(fname)
         print("\n".join([json.dumps(r) for r in results]))
     else:
         print("Unknown file format.")
